37bc6548a7773ef03dd960ae37a28d35dab6865b
IohannesPaullusMD <141614774+IohannesPaullusMD@users.noreply.github.com>
2025-04-02 11:12:07 +0800

    add: initial routing

---
+++ views/app.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My App</title>
+
+</head>
+
+<body>
+    <header>
+        <h1>Welcome to My App</h1>
+    </header>
+    <main>
+        <div id="app">
+        </div>
+    </main>
+    <footer>
+        <p>&copy; 2025 My App</p>
+    </footer>
+</body>
+
+</html>
